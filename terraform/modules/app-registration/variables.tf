38e30db8c98c2b16986aee16b4d21a46df1ad5bf
michaelcorrodus <admin@iamconnect.co.uk>
2025-11-14 18:26:28 +0000

    Update variables.tf

--- terraform/modules/app-registration/variables.tf
+++ terraform/modules/app-registration/variables.tf
@@ -1,14 +1,41 @@
-variable "display_name" { type = string }
+variable "display_name" {
+  type = string
+}
 
-variable "enable_web"    { type = bool default = true }
-variable "enable_spa"    { type = bool default = false }
-variable "enable_native" { type = bool default = false }
+variable "enable_web" {
+  type    = bool
+  default = true
+}
 
-variable "web_redirect_uris" { type = list(string) default = [] }
-variable "web_logout_url"    { type = string      default = null }
+variable "enable_spa" {
+  type    = bool
+  default = false
+}
 
-variable "spa_redirect_uris"    { type = list(string) default = [] }
-variable "native_redirect_uris" { type = list(string) default = [] }
+variable "enable_native" {
+  type    = bool
+  default = false
+}
+
+variable "web_redirect_uris" {
+  type    = list(string)
+  default = []
+}
+
+variable "web_logout_url" {
+  type    = string
+  default = null
+}
+
+variable "spa_redirect_uris" {
+  type    = list(string)
+  default = []
+}
+
+variable "native_redirect_uris" {
+  type    = list(string)
+  default = []
+}
 
 variable "app_roles" {
   type = list(object({
