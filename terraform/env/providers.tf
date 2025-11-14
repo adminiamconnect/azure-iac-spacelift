35b67b12e6a95100163ae043e0888d6811ef1714
michaelcorrodus <admin@iamconnect.co.uk>
2025-11-14 18:31:49 +0000

    Update providers.tf

--- terraform/env/providers.tf
+++ terraform/env/providers.tf
@@ -2,17 +2,6 @@ variable "tenant_id" {
   type = string
 }
 
-variable "subscription_id" {
-  type = string
-}
-
-provider "azurerm" {
-  features {}
-  use_oidc             = true
-  oidc_token_file_path = "/mnt/workspace/spacelift.oidc"
-  subscription_id      = var.subscription_id
-}
-
 provider "azuread" {
   tenant_id            = var.tenant_id
   use_oidc             = true
